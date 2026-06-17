000366c1c187d8bc9387dd6ca34263978795ec19
codeHasib <mohammadhasib9827@gmail.com>
2026-06-17 19:46:03 +0600

    db connected

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
@@ -9,10 +10,23 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = `${process.env.PORT}`;
+const uri = process.env.DB_URI;
 
-app.get("/", (req, res) => {
-  res.send("Welcome to Routemate api");
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("routemate");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
