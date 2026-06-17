2f46779fdd0541c1a7d624eaa92bd377e49a32b5
codeHasib <mohammadhasib9827@gmail.com>
2026-06-17 20:30:27 +0600

    add middleware for jwt setup

--- index.js
+++ index.js
@@ -2,8 +2,37 @@ const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
 dotenv.config();
+
+// middleware setup for authentication
+const { jwtVerify, createRemoteJWKSet } = require("jose-cjs");
+
+const JWKS_URL = `${process.env.NEXTJS_AUTH_JWKS_URL}/api/auth/jwks`;
+const JWKS = createRemoteJWKSet(new URL(JWKS_URL));
+
+const authenticateToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Access Token missing." });
+    }
+
+    const { payload } = await jwtVerify(token, JWKS, { algorithms: ["RS256"] });
+
+    // Attach everything to req.user (id, email, role)
+    req.user = payload;
+    next();
+  } catch (error) {
+    return res
+      .status(403)
+      .json({ success: false, message: "Token is invalid." });
+  }
+};
+///////////////////////////////////
 
 const app = express();
 app.use(cors());
@@ -23,6 +52,11 @@ async function run() {
   try {
     await client.connect();
     const db = client.db("routemate");
+
+    app.get("/test", authenticateToken, (req, res) => {
+      res.send("Hello hi");
+    });
+    
   } finally {
   }
 }
